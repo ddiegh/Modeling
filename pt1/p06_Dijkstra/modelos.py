c60d49946fec69312f9120f812b219708163ce9b
Diego Sanchez <sanchez97sr@gmail.com>
2026-07-20 15:47:55 -0600

    feat: funciones de reconstruccion de rutas, mostrarlas y distancia entre puntos

--- pt1/p06_Dijkstra/modelos.py
+++ pt1/p06_Dijkstra/modelos.py
@@ -46,25 +46,66 @@ def dijkstra(A:list[list], o:int)->tuple:
             if min(distancias[pv], etiqueta) == etiqueta:
                 predecesores[pv] = NF
 
-        #me marcaba ese error 
-        if len ([distancias[pv] for pv in posibles_visitas]) == 0:
-            break
 
-        else: 
-            #borramos las conexiones del nodo fijo porque ya es permanente 
-            for i in range(n): 
-                A[NF][i] = 0
-                A[i][NF] = 0
+        #borramos las conexiones del nodo fijo porque ya es permanente 
+        for i in range(n): 
+            A[NF][i] = 0
+            A[i][NF] = 0
 
-            #emparejamos los posibles nodos permanentes con el costo que tienen para decidir
-            visit_valor = zip([na for na in nodos_abiertos], [distancias[na] for na in nodos_abiertos])
-            
-            Nuev_nodo, x = min(visit_valor, key = lambda x: x[1])
-            
-            #actualizamos
-            permanentes.append(Nuev_nodo)
-            nodos_abiertos.remove(Nuev_nodo)
+        #emparejamos los posibles nodos permanentes con el costo que tienen para decidir
+        visit_valor = zip([na for na in nodos_abiertos], [distancias[na] for na in nodos_abiertos])
+
+        Nuev_nodo, x = min(visit_valor, key = lambda x: x[1])
+
+        #actualizamos
+        permanentes.append(Nuev_nodo)
+        nodos_abiertos.remove(Nuev_nodo)
 
     return distancias, predecesores
 
+
+
+def reconstruir_ruta(A:list[list], o:int):
+    """
+    Reconstrucción de las rutas más cortas del algoritmo dijkstra usando backtracking
+    """
+
+    distancias, predecesores = dijkstra(A, o)
+
+    todas_las_rutas = []
+    for i, predecesor in enumerate(predecesores):
+        
+        rutas = [predecesor]
+        anterior = predecesor
+        
+        #la ruta debe empezar en el nodo inicial. 
+        while rutas[-1] != o:  
+            anterior = predecesores[rutas[-1]]
+            rutas.append(anterior)
+        
+        #agregamos el nodo final 
+        rutas.insert(0, i)
+        rutas.reverse()
+        todas_las_rutas.append(rutas)
+
+    return todas_las_rutas, distancias
+
+
+def mostrar_rutas(A:list[list], o:int):
+    """
+    Mostrar las rutas de dijkstra de una forma más simple de entender.
+    """
+
+    rutas, distancias = reconstruir_ruta(A, o)
+
+    for i in range(len(rutas)):
+        print(f" La distancia del nodo inicial {o} al nodo {i} es de {distancias[i]} siguiendo la ruta: {rutas[i]}")
+
+
+def distancia_puntos(A:list[list], a:int, b:int):
+    """
+    Distancia minima entre dos puntos usando el algoritmo de dijkstra.
+    """
+    rutas, distancias = reconstruir_ruta(A, a)
+    print(f"la distancia mas corta del punto {a} a {b} es de {distancias[b]} siguiendo la ruta {rutas[b]}")
 
